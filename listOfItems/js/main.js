fc7391aae2e7604b593a4c2ec6d5cbbf42f60b2d
Luque <jose.luque@mrm-mccann.com>
2016-10-19 18:36:49 -0300

    ajax form

--- listOfItems/js/main.js
+++ listOfItems/js/main.js
@@ -1,3 +1,21 @@
+/*var serializeObject = function () {
+	var oJson = {},
+		oArray = this.serializeArray();
+	
+	$.each(a, function(){
+		if(oJson[this.name] !== undefined){
+			if(!oJson[this.name].push){
+				oJson[this.name] = [oJson[this.name]];
+			}
+			oJson[this.name].push(this.value || '');
+		}else{
+			oJson[this.name] = this.name || '';
+		}
+	});
+
+	return oJson;
+}*/
+
 $.getJSON("games.json", function(json) {
 	var output = '<ul id="sortable1" class="connectedSortable">';
 	for (i in json){
@@ -22,6 +40,8 @@ $.getJSON("games.json", function(json) {
 	/*var count = Object.keys(json).length;
 	console.log(json);*/
 });
+
+/* http://jsfiddle.net/sxGtM/3/ --> save json */
 $(function(){
 	$("#sortable1").sortable({
 		connectWith: ".connectedSortable"
@@ -40,4 +60,45 @@ $(function(){
 	$('.ui-sortable-handle .delete').on("click", function(){
 		$(this).parent().parent().parent().parent().hide()
 	})
+
+    /*$('#formAdd').submit(function() {
+        $('#result').text(JSON.stringify($('form').serializeObject()));
+        return false;
+    });*/
+});
+
+$("#formAdd").submit(function(e){
+
+    e.preventDefault();
+
+    var data = {}
+    var Form = this;
+
+    //Gathering the Data
+    //and removing undefined keys(buttons)
+    $.each(this.elements, function(i, v){
+		var input = $(v);
+		data[input.attr("name")] = input.val();
+		delete data["undefined"];
+    });
+
+    //Form Validation goes here....
+
+    //Save Form Data........
+    $.ajax({
+        cache: false,
+        url : "http://localhost/git/practices/listOfItems/",
+        type: "POST",
+        dataType : "json",
+        data : JSON.stringify(data),
+        context : Form,
+        success : function(callback){
+            //Where $(this) => context == FORM
+            console.log(JSON.parse(callback));
+            $(this).html("Success!");
+        },
+        error : function(){
+            $(this).html("Error!");
+        }
+    });
 });
