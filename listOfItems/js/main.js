8e03cba26f52a7a4c17fed06588bc7acba23e4b7
Luque <jose.luque@mrm-mccann.com>
2016-10-19 14:35:56 -0300

    list dinamic json

--- listOfItems/js/main.js
+++ listOfItems/js/main.js
@@ -1,11 +1,31 @@
+$.getJSON("games.json", function(json) {
+	var output = '<ul id="sortable1" class="connectedSortable">';
+	for (i in json){
+		output += '<li>';
+		output += 	'<div class="row boxArticles white">';
+		output += 		'<div class="col l2 imgArticles">';
+		output += 			'<img class="responsive-img" src="' + json[i].img + '" alt="">';
+		output += 		'</div>';
+		output += 		'<div class="col l10 descriptionArticles">';
+		output += 			'<div class="panel">';
+		output += 				'<i class="small material-icons edit">mode_edit</i>';
+		output += 				'<i class="small material-icons delete">delete</i>';
+		output += 			'</div>';
+		output += 			'<b>' + json[i].title + '</b>';
+		output += 			'<p>' + json[i].description + '</p>';
+		output += 		'</div>';
+		output += 	'</div>';
+		output += '</li>';
+	}
+	output += '</ul>';
+	$('.containerGames').append(output);
+	/*var count = Object.keys(json).length;
+	console.log(json);*/
+});
 $(function(){
 	$("#sortable1").sortable({
 		connectWith: ".connectedSortable"
     }).disableSelection();
-
-	$.getJSON("games.json", function(json) {
-		console.log(json); // this will show the info it in firebug console
-	});
 
 	$('.ui-sortable-handle')
 		.mouseenter(function(){
